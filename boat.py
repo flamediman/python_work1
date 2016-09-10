9aa4687862221e99023c04f720fdf98617711903
Дмитрий Поспелов <DPospelov@MacBook-Pro-Dmitrij.local>
2016-09-10 17:07:24 +0300

    new commit

--- boat.py
+++ boat.py
@@ -4,5 +4,5 @@ s_boat = float(input('введите скорость лодки '))
 width = float(input('введите ширину реки '))
 time = width / s_boat
 way_r = time * s_river
-way = (width ** 2) + (way_r ** 2)
-print(math.sqrt(way))
+all_way = (width ** 2) + (way_r ** 2)
+print(math.sqrt(all_way))
